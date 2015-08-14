30b3ecd955b1dda88347c5179cd47adafa3a61e0
Christopher Mark Gore <cgore@cgore.com>
2015-08-14 14:22:51 -0500

    Rolling forward clojure and clj-time versions.

--- project.clj
+++ project.clj
@@ -5,25 +5,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [;;Use clojure 1.3 for pom generation
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.7.0"]
                  [ring-mock "0.1.5"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.apache.httpcomponents/httpmime "4.1.3"
                   :exclusions [commons-logging]]
-                 [clj-time "0.9.0"]]
+                 [clj-time "0.11.0"]]
   :profiles {:dev {:dependencies [[net.cgrand/moustache "1.1.0"
                                     :exclusions
                                     [[org.clojure/clojure]
                                      [ring/ring-core]]]
                                    [ring/ring-core "1.3.2"]
-                                   [javax.servlet/servlet-api "2.5"]
-                                   ;; use 1.6 for development
-                                   ;; cider requires it
-                                   ^:replace [org.clojure/clojure "1.6.0"]]
+                                   [javax.servlet/servlet-api "2.5"]]
                    :plugins [[lein-ancient "0.6.7"]]
                    :resource-paths ["test-resources"]}
              ;; use the relevant clojure version for testing
-             :1.3 {:dependencies [^:replace [org.clojure/clojure "1.3.0"]]}
+             :1.3 {:dependencies [^:replace [org.clojure/clojure "1.3.0"]
+                                  ^:replace [clj-time "0.10.0"]]}
              :1.4 {:dependencies [^:replace [org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [^:replace [org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [^:replace [org.clojure/clojure "1.6.0"]]}}
